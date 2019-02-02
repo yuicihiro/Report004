d7ba5382817cb9145804edc595a877274ad93976
Yuichiro Nakamine <e185726@ie.u-ryukyu.ac.jp>
2019-02-03 03:01:23 +0900

    second

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ public class Main {
     public static void  main(String args[]) {
 
 
-        String str = null;
-        System.out.println(str.length());
+       
+         try {
+               String str = null;
+                System.out.println(str.length());
+            
+                System.out.println("適切に処理されました。");
+                        
+         
+         } catch (NullPointerException e) {
+                System.out.println("例外が発生しました。");
+                System.out.println(e.getMessage());
+            
+             
+         }
     }
 }
