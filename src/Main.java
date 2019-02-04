49c51466865a022fe7d6d99f86c2e5207571a95d
Yuichiro Nakamine <e185726@ie.u-ryukyu.ac.jp>
2019-02-04 13:15:42 +0900

    third

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
+
 public class Main {
-    public static void  main(String args[]) {
-
-
-       
-         try {
-               String str = null;
-                System.out.println(str.length());
-            
-                System.out.println("適切に処理されました。");
-                        
-         
-         } catch (NullPointerException e) {
-                System.out.println("例外が発生しました。");
-                System.out.println(e.getMessage());
-            
-             
-         }
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(" string = " + value);
     }
 }
